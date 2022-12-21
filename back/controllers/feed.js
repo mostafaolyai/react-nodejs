ce83160dd0d2850c4f8fd5949f2a01fbf1fe3520
mostafa <amarali2008@gmail.com>
2022-12-21 10:31:44 +0330

    insert post

--- back/controllers/feed.js
+++ back/controllers/feed.js
@@ -1,15 +1,49 @@
+const { validationResult } = require('express-validator')
+
+const Post = require('../models/post')
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [{
+      _id: 1, 
+      title: 'First Post', 
+      content: 'This is the first post!',
+      creator: {
+        name:"mosi",
+      },
+      createdAt: new Date() 
+    }]
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    res.status(422).json({
+      message: 'Validation Failed!',
+      errors: errors.array()
+    })
+  }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
-  });
+  const imageUrl = 'images/duck';
+  
+  const post = new Post({
+    title,
+    content,
+    imageUrl,
+    creator:{
+      name:'mosi'
+    }
+  })
+  post
+    .save()
+    .then(result => {
+      console.log(result)
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result
+      });
+    })
+    .catch(err => console.log(err));
 };
--- front/src/pages/Feed/Feed.js
+++ front/src/pages/Feed/Feed.js
@@ -22,7 +22,7 @@ class Feed extends Component {
   };
 
   componentDidMount() {
-    fetch('URL')
+    fetch('http://localhost:8080/feed/posts')
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch user status.');
@@ -50,7 +50,7 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('URL')
+    fetch('http://localhost:8080/feed/posts')
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch posts.');
@@ -69,7 +69,7 @@ class Feed extends Component {
 
   statusUpdateHandler = event => {
     event.preventDefault();
-    fetch('URL')
+    fetch('http://localhost:8080/feed/posts')
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Can't update status!");
@@ -106,12 +106,22 @@ class Feed extends Component {
       editLoading: true
     });
     // Set up data (with image!)
-    let url = 'URL';
+    let url = 'http://localhost:8080/feed/post';
+    let method = 'POST';
     if (this.state.editPost) {
       url = 'URL';
     }
 
-    fetch(url)
+    fetch(url, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: postData.title,
+        content: postData.content
+      })
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error('Creating or editing a post failed!');
