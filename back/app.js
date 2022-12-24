88cfb4fbe2bb5291cb44b342c24f2a452dd5dec5
mostafa <amarali2008@gmail.com>
2022-12-24 08:39:10 +0330

    add log

--- back/app.js
+++ back/app.js
@@ -1,17 +1,28 @@
 const express = require('express');
+const fs = require('fs')
+const path = require('path')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+//
 const helmet = require('helmet')
 const compression = require('compression')
-
+const morgan = require('morgan')
+//
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+
 app.use(helmet())
 app.use(compression())
+const accesslogStream = fs.createWriteStream(
+  path.join(__dirname, 'access.log'),
+  { flags:'a' }
+)
+app.use(morgan('combined',{ stream: accesslogStream }))
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
